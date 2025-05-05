0b8433f1d139ccd5f3e55c85610f4ca361742fd4
Dominik Dietz <dom.dietz@gmx.de>
2025-05-05 17:23:42 +0200

    adjust hero section, add custom classes

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -4,21 +4,20 @@ import HeroImg from "@/assets/images/hero.jpg";
 
 const Hero = () => {
   return (
-    <section className="pt-32 pb-20 md:pt-40 md:pb-28">
-      <div className="container mx-auto px-4">
+    <section className="pt-16 pb-20 md:pt-40 md:pb-28">
+      <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 md:py-24 lg:px-8">
         <div className="flex flex-col items-center gap-10 md:flex-row">
-          <div className="w-full space-y-6 md:w-1/2">
+          <div className="space-y-6">
             <div>
-              <p className="text-primary mb-3 text-lg font-medium">
-                Hi! I am Dominik
+              <p className="text-3xl leading-tight font-bold md:text-5xl lg:text-5xl">
+                Hi! I am{" "}
+                <span className="text-sky-700 dark:text-blue-400">Dominik</span>
               </p>
-              <h1 className="text-3xl leading-tight font-bold md:text-5xl lg:text-5xl">
-                <span className="bg-gradient-to-r bg-clip-text">
-                  Software Developer
-                </span>
+              <h1 className="text-primary mb-3 text-lg md:text-2xl">
+                Software Developer
               </h1>
-              <p className="mt-6 text-lg text-gray-600 md:text-xl dark:text-gray-300">
-                Bridging design and functionality with modern JavaScript
+              <p className="text-primary/80 mt-6 text-lg md:text-xl">
+                Bridging design and functionality with modern Javascript
                 frameworks
               </p>
             </div>
@@ -27,7 +26,8 @@ const Hero = () => {
               <a
                 href="https://github.com"
                 target="_blank"
-                className="hover:text-primary dark:hover:text-primary text-gray-600 transition-colors dark:text-gray-400"
+                rel="noopener noreferrer"
+                className="dark:hover:text-primary text-gray-800 transition-transform hover:scale-110 dark:text-gray-200"
                 aria-label="GitHub"
               >
                 <GithubIcon />
@@ -35,7 +35,8 @@ const Hero = () => {
               <a
                 href="https://linkedin.com"
                 target="_blank"
-                className="hover:text-primary dark:hover:text-primary text-gray-600 transition-colors dark:text-gray-400"
+                rel="noopener noreferrer"
+                className="dark:hover:text-primary text-gray-800 transition-transform hover:scale-110 dark:text-gray-200"
                 aria-label="LinkedIn"
               >
                 <LinkedinIcon />
@@ -43,11 +44,11 @@ const Hero = () => {
             </div>
           </div>
 
-          <div className="w-full md:w-1/2">
+          <div className="flex justify-center">
             <img
               src={HeroImg.src}
               alt="Hero Image"
-              className="shadow-accent aspect-square h-full w-full rounded-full border-4 border-sky-900 object-cover shadow-2xl"
+              className="aspect-square max-h-96 rounded-full border-3 border-sky-700 object-cover dark:border-blue-400"
             />
           </div>
         </div>
