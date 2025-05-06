cc29d9dfd32c1fba1830638145183cf4e170d1ba
Dominik Dietz <dom.dietz@gmx.de>
2025-05-06 17:30:46 +0200

    fix alternate section bg color

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -15,25 +15,24 @@ const Section = ({
   children,
   isAlternate,
 }: SectionProps) => {
-  const sectionClass = cn(
-    "mx-auto max-w-7xl px-4 py-16 sm:px-6 md:py-24 lg:px-8",
-    {
-      "bg-muted/30": isAlternate,
-    },
-  );
+  const sectionClass = cn({
+    "bg-muted/30": isAlternate,
+  });
 
   return (
     <section id={id} className={sectionClass}>
-      <h2 className="mb-8 text-center text-3xl font-bold md:text-4xl">
-        {title}
-      </h2>
+      <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 md:py-24 lg:px-8">
+        <h2 className="mb-8 text-center text-3xl font-bold md:text-4xl">
+          {title}
+        </h2>
 
-      {subtitle && (
-        <p className="text-muted-foreground mx-auto mb-12 max-w-3xl text-center text-xl">
-          {subtitle}
-        </p>
-      )}
-      {children}
+        {subtitle && (
+          <p className="text-muted-foreground mx-auto mb-12 max-w-3xl text-center text-xl">
+            {subtitle}
+          </p>
+        )}
+        {children}
+      </div>
     </section>
   );
 };
